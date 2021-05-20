e149d92ef4bd996522640bb748ff8e0282c22a57
Carson <jackparadise520a@gmail.com>
2021-05-20 16:26:08 +0800

    Editing behavior changed to the "x-editable" (#15)

--- app/urls/bs-table.go
+++ app/urls/bs-table.go
@@ -1,14 +1,22 @@
 package urls
 
 import (
+    "github.com/CarsonSlovoka/excel/app"
     "github.com/CarsonSlovoka/excel/app/server"
     "net/http"
 )
 
-
 func init() {
     ht := NewTemplate("bs-table.go.html", fsTemplates, "templates/base.go.html", "templates/file/bs-table.go.html")
-    ht.contextSet = append(ht.contextSet, BaseContext)
+
+    ht.contextSet = append(ht.contextSet, Context{
+        "Site": Setting{
+            EnableBootstrap: false, // We need to use x-editable, and unfortunately, it can't support the newest version.
+            EnableJquery: true, EnableFontawesome: true,
+        },
+        "Author":  app.Author,
+        "Version": app.Version,
+    })
 
     server.Mux.HandleFunc("/bs-table/", func(w http.ResponseWriter, r *http.Request) {
         ht.ServeHTTP(w, r)
--- app/urls/static/js/src/file/file.js
+++ app/urls/static/js/src/file/file.js
@@ -3,6 +3,7 @@ import {ReadFile} from "../../pkg/io/ioutil.js"
 
 const IFRAME_ID = "iframe-bootstrap-table"
 const UNIQUE_ID = "__id__"
+const BUTTONS_CLASS = "primary"
 
 function removeExtraSpace(stringData) {
   stringData = stringData.replace(/,( *)/gm, ",")  // remove extra space
@@ -42,121 +43,159 @@ function save2Server() {
   xhr.send(formData)
 }
 
-function createBSTable(dataArray) {
-  const div = document.getElementById('div-csv-data')
-  div.querySelectorAll('*').forEach(node => node.remove())
-  const iframe = document.createElement('iframe')
-  iframe.id = IFRAME_ID
-  iframe.height = "100%"
-  iframe.width = "100%"
-  iframe.src = "/bs-table/"
-  div.append(iframe)
-  // iframe.contentWindow.location.reload(false)
+class BSTable {
+  constructor(dataArray) {
+    this.dataArray = dataArray
+    const div = document.getElementById('div-csv-data')
+    div.querySelectorAll('*').forEach(node => node.remove())
+    this.table = undefined
+    this.iframe = this.newIframe()
+    this.initIframeEvent()
+    div.append(this.iframe)
+  }
 
-  iframe.onload = (event) => {
-    const iframeCtxWin = iframe.contentWindow
-    const iframeCtxWinDoc = iframeCtxWin.document
+  newIframe() {
+    const iframe = document.createElement('iframe')
+    iframe.id = IFRAME_ID
+    iframe.height = "100%"
+    iframe.width = "100%"
+    iframe.src = "/bs-table/"
+    return iframe
+  }
 
-    // const TABLE_ID = iframeCtxWinDoc.querySelector("table").getAttribute("id")
-    // const TABLE = iframeCtxWinDoc.getElementById(TABLE_ID)
-    const TABLE = iframeCtxWin.TABLE
-    const table = iframeCtxWin.table // iframeCtxWin.[my-variable] // $("#iframe-bootstrap-table").contents().find(`[id=${TABLE_ID}]`) bootstrap-table導入的時候table就會消失會取不到
+  initIframeEvent() {
+    this.iframe.onload = (event) => {
+      const iframeCtxWin = this.iframe.contentWindow // ★ contentWindow must wait until the HTML is  loaded before it will work.
+      const iframeCtxWinDoc = iframeCtxWin.document
+      // const TABLE_ID = iframeCtxWinDoc.querySelector("table").getAttribute("id")
+      // const TABLE = iframeCtxWinDoc.getElementById(TABLE_ID)
+      // this.TABLE = iframeCtxWin.TABLE
+      this.table = iframeCtxWin.table // iframeCtxWin.[my-variable] // $("#iframe-bootstrap-table").contents().find(`[id=${TABLE_ID}]`) bootstrap-table導入的時候table就會消失會取不到
 
-    if (dataArray.length === 0) {
-      return
-    }
-    const headers = []
-    const firstObj = dataArray[0]
-    for (const [key, value] of Object.entries(firstObj)) {
-      headers.push(key)
-    }
-    dataArray = dataArray.map((obj, idx) => (obj[UNIQUE_ID] = idx, obj)) // add serial number
-
-    // [refresh bs-table](https://github.com/wenzhixin/bootstrap-table/issues/64)
-    const columns = headers.map(e => ({field: e, title: e, sortable: "true"}))
-    columns.splice(0, 0,
-      {checkbox: true, width: 64, align: 'center'}, // Add a checkbox to select the whole row.
-      // {field: UNIQUE_ID, title: "uid", visiable: false, sortable: true} // new column for UNIQUE_ID // It's ok if you aren't gonna show it to the user. Data still save on the datatable, no matter you set the filed or not.
-    )
-    table.bootstrapTable('refreshOptions',
-      {
-        columns: columns, // [{field: "Name", title: "名稱"}, {field: "Desc", title: "說明"}]
-        // url: dataURL // You can use ``url`` instead of ``data``, but there is unnecessary since we already get all the data.
-        data: dataArray,
-        height: 768,
-        uniqueId: UNIQUE_ID, // Using the ``headers[0]`` is not a great idea, so I create a column(__id__) instead of it. // data-unique-id
-        buttons: { // data-buttons
-          btnDeleteSelect: {
-            text: 'Delete',
-            icon: 'fa-trash-alt',
-            attributes: {
-              style: 'color:#82c91e',
-              title: 'Delete all selection data'
-            },
-            event: {
-              'click': () => {
-                dataArray = table.bootstrapTable('getSelections')
-                const ids = []
-                dataArray.forEach(obj => {
-                    ids.push(obj[UNIQUE_ID])
-                  }
-                )
-                table.bootstrapTable('remove', {
-                  field: UNIQUE_ID,
-                  values: ids,
-                })
-              }
-            }
-          },
-          btnUsersAdd: {
-            text: 'Save',
-            icon: 'fa-save',
-            attributes: {
-              title: 'Save data'
-            },
-            event: {
-              'click': () => {
-                save2Server()
-              },
-              'mouseenter': () => {
-              },
-              'mouseleave': () => {
-              }
-            },
-          }
-        }
-      }
-    )
-    // table.uniqueId = UNIQUE_ID // Add other attributes for bootstrap-table
-    table.on('click-cell.bs.table', function (event, field, value, rowObj, td) {
-      td = td[0]
-      if (td.getAttribute("contentEditable")) {
+      if (this.dataArray.length === 0) {
         return
       }
-      const index = Array.prototype.indexOf.call(td.parentNode.children, td)
-      td.contentEditable = "true"
+      const headers = []
+      const firstObj = this.dataArray[0]
+      for (const [key, value] of Object.entries(firstObj)) {
+        headers.push(key)
+      }
+      this.dataArray = this.dataArray.map((obj, idx) => (obj[UNIQUE_ID] = idx, obj)) // add serial number
 
-      td.addEventListener("keyup", (event) => {
-        clearTimeout($.data(this, 'timer'))
-        const id = rowObj[UNIQUE_ID]
-        const wait = setTimeout(saveChange, 1500, td, id, field) // delay after user types
-        $(this).data('timer', wait)
+      // [refresh bs-table](https://github.com/wenzhixin/bootstrap-table/issues/64)
+      const columns = headers.map(e => ({field: e, title: e, sortable: "true",
+        editable: {
+          type: 'text',
+          title: e,
+          emptytext: "",
+          validate: function(v) { if(!v) return "You can't set the null value on this column" },
+        }
+      }))
+      columns.splice(0, 0,
+        {checkbox: true, width: 64, align: 'center'}, // Add a checkbox to select the whole row.
+        // {field: UNIQUE_ID, title: "uid", visiable: false, sortable: true} // new column for UNIQUE_ID // It's ok if you aren't gonna show it to the user. Data still save on the datatable, no matter you set the filed or not.
+      )
+      columns.push({
+        field: "tableAction", title: "Action", align: "center", width:64,
+        formatter: (value, row, index, field) => {
+          const curID = row[UNIQUE_ID]
+          return [
+            `<button type="button" class="btn btn-default btn-sm" onclick="deleteItem(${curID})">`,
+            `<span class="glyphicon glyphicon-trash"></span>`,
+            `</button>`
+          ].join('')
+        }
       })
-    })
-    table.bootstrapTable('refresh')
+      this.initToolbar()
+      this.table.bootstrapTable('refreshOptions',
+        {
+          columns: columns, // [{field: "Name", title: "名稱"}, {field: "Desc", title: "說明"}]
+          // url: dataURL // You can use ``url`` instead of ``data``, but there is unnecessary since we already get all the data.
+          data: this.dataArray,
+          height: 768,
+          uniqueId: UNIQUE_ID, // Using the ``headers[0]`` is not a great idea, so I create a column(__id__) instead of it. // data-unique-id
+          clickToSelect: true,
+          buttonsClass: BUTTONS_CLASS,
+        }
+      )
+      // table.uniqueId = UNIQUE_ID // Add other attributes for bootstrap-table
+      this.table.bootstrapTable('refresh')
+    }
   }
-}
 
-function saveChange(td, id, field) {
-  // field: which column you want to update.
-  const bsTable = document.getElementById(IFRAME_ID).contentWindow.table
-  // const th = getTh(td, bsTable.attr("id"))
-  bsTable.bootstrapTable('updateCellByUniqueId', {id, field, value: td.innerHTML})
-}
+  initToolbar() {
+    const divToolbar = this.iframe.contentWindow.document.getElementById("toolbar")
+    divToolbar.setAttribute("style", "padding-left:0.4em")
+    divToolbar.setAttribute("class", "columns columns-right btn-group float-right")
+    let button
+    let icon
 
-function getTh(td, tableID) {
-  const index = Array.prototype.indexOf.call(td.parentNode.children, td)
-  return document.getElementById(IFRAME_ID).contentWindow.document.querySelector(`#${tableID} th:nth-child( ${(index + 1)} )`)
+    const buttonsObj = {
+      btnDeleteSelect: {
+        text: 'Delete',
+        icon: 'fa-trash-alt',
+        attributes: {
+          style: 'color:#ff0000',
+          title: 'Delete all selection data'
+        },
+        event: {
+          'click': () => {
+            const selectDataArray = this.table.bootstrapTable('getSelections')
+            const ids = []
+            selectDataArray.forEach(obj => {
+                ids.push(obj[UNIQUE_ID])
+              }
+            )
+            this.table.bootstrapTable('remove', {
+              field: UNIQUE_ID,
+              values: ids,
+            })
+          }
+        }
+      },
+      btnUsersAdd: {
+        text: 'Save',
+        icon: 'fa-save',
+        attributes: {
+          title: 'Save data'
+        },
+        event: {
+          'click': () => {
+            alert("save data to server")
+          },
+          'mouseenter': () => {
+          },
+          'mouseleave': () => {
+          }
+        },
+      }
+    }
+
+    for (const [key, btnObj] of Object.entries(buttonsObj)) {
+      const iconClass = btnObj.icon
+      button = document.createElement("button")
+      button.setAttribute("class", `btn btn-${BUTTONS_CLASS}`)
+      button.setAttribute("style", btnObj.attributes.style)
+      button.setAttribute("title", btnObj.attributes.title)
+      button.addEventListener("click", btnObj.event.click)
+      icon = document.createElement("i")
+      icon.setAttribute("class", `far ${iconClass}`)
+      button.appendChild(icon)
+      divToolbar.appendChild(button)
+    }
+  }
+
+  deleteItem(curID) {
+    if (!confirm('Are you sure you want to delete this item?')) {
+      return
+    }
+
+    const ids = [curID]
+    this.table.bootstrapTable('remove', {
+      field: UNIQUE_ID,
+      values: ids,
+    })
+  }
 }
 
 (
@@ -178,7 +217,7 @@ function getTh(td, tableID) {
         promise.then(fileContent => {
           fileContent = removeExtraSpace(fileContent)
           const dataArray = $.csv.toObjects(fileContent)
-          createBSTable(dataArray)
+          const bsTable = new BSTable(dataArray)
         })
       }
     }
